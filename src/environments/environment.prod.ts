c6529f007cd51004fd49f9132acd9c5cc2784d09
guilhermerfs00 <guilherme.rfs00@gmail.com>
2025-04-16 14:14:57 -0300

    Configura deploy frontend Angular no Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://apptite-tcc.herokuapp.com'
-  };
-  
+  production: true,
+  apiUrl: 'https://apptite-back-6b87ad4804d1.herokuapp.com'
+};
