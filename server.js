d5983c8ed7bbedb7100f75ef79f7f24e6d7705aa
guilhermerfs00 <guilherme.rfs00@gmail.com>
2025-04-16 15:16:18 -0300

    Corrige CSP para permitir backend Heroku

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const path = require('path');
-const helmet = require('helmet');
+
 const app = express();
 
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      "default-src": ["'self'"],
-      "script-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
-      "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
-      "font-src": ["'self'", "https://fonts.gstatic.com"],
-      "connect-src": ["'self'", "https://apptite-back-6b87ad4804d1.herokuapp.com"], // Libera chamadas ao backend
-      "img-src": ["'self'", "data:"],
-    },
-  })
-);
+// Middleware para configurar CSP corretamente
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "font-src https://fonts.gstatic.com; " +
+    "connect-src 'self' https://apptite-back-6b87ad4804d1.herokuapp.com;"
+  );
+  next();
+});
 
+// Serve os arquivos estáticos do Angular
 app.use(express.static(path.join(__dirname, 'dist/apptite-front/browser')));
 
+// Rota fallback para SPA
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/apptite-front/browser/index.html'));
 });
 
+// Inicializa o servidor
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
